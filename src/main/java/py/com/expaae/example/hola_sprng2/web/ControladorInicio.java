3a7faec224c79237bdb4fffabc36b5d344f66df4
Alfonso Etcheverry <alfonso.alej@gmail.com>
2021-09-22 10:12:31 -0400

    leccion 18

--- src/main/java/py/com/expaae/example/hola_sprng2/web/ControladorInicio.java
+++ src/main/java/py/com/expaae/example/hola_sprng2/web/ControladorInicio.java
@@ -4,9 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
- 
+import org.springframework.web.bind.annotation.PostMapping;
+
 import lombok.extern.slf4j.Slf4j;
-import py.com.expaae.example.hola_sprng2.dao.PersonaDao;
+import py.com.expaae.example.hola_sprng2.domain.Persona;
 import py.com.expaae.example.hola_sprng2.servicio.PersonaService;
 
 @Controller
@@ -23,5 +24,18 @@ public class ControladorInicio {
         log.debug("debugeando el servicio HOLA MVC");
         model.addAttribute("personas", personas);
         return "index";
+    }
+
+    @GetMapping("/agregar")
+    public String agregar (Persona persona){        
+        log.info("Iniciando controlador AGREGAR del Spring MVC");        
+        return "modificar";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar (Persona persona){
+        log.info("Iniciando controlador AGREGAR del Spring MVC");
+        personaService.guardar(persona);
+        return "redirect:/";
     }
 }
